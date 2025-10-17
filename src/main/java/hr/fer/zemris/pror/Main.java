df77cd188d504cdb83520bc6b16cbaa4e5aa61e6
Nino Strčić <57328197+NStrcic@users.noreply.github.com>
2025-10-18 01:36:04 +0200

    Napravljen osnovni model kretanja patke u ravnini, patku predstavljaju *, vodu # a zrak -

---
+++ src/main/java/hr/fer/zemris/pror/Main.java
@@ -0,0 +1,70 @@
+package hr.fer.zemris.pror;
+
+import java.awt.*;
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        int rows = 11, cols = 82;
+        char[][] grid = new char[rows][cols];
+
+        // fill with '-'
+        for (int r = 0; r < rows; r++) {
+            java.util.Arrays.fill(grid[r], '-');
+        }
+        for (int r = rows - 5; r < rows; r++) {
+            java.util.Arrays.fill(grid[r], '#');
+        }
+
+        Point topLeft = new Point(2, 4);
+        //duck square
+        for (int i = topLeft.x; i < topLeft.x + 3; i++) {
+            for (int j = topLeft.y; j < topLeft.y + 3; j++) {
+                grid[j][i] = '*';
+            }
+        }
+        // print it
+        for (int r = 0; r < rows; r++) {
+            System.out.println(new String(grid[r]));
+        }
+        Scanner sc = new Scanner(System.in);
+        char inp = sc.next().charAt(0);
+        while (inp != 's') {
+            for (int r = 0; r < rows; r++) {
+                java.util.Arrays.fill(grid[r], '-');
+            }
+            for (int r = rows - 5; r < rows; r++) {
+                java.util.Arrays.fill(grid[r], '#');
+            }
+            switch (inp) {
+                case 'f':
+                    topLeft.setLocation(topLeft.x+1, topLeft.y);
+                    break;
+                case 'b':
+                    topLeft.setLocation(topLeft.x-1, topLeft.y);
+                    break;
+                case 'u':
+                    topLeft.setLocation(topLeft.x, topLeft.y-1);
+                    break;
+                case 'd':
+                    topLeft.setLocation(topLeft.x, topLeft.y+1);
+                    break;
+                case 'n':
+                    topLeft.setLocation(topLeft.x, topLeft.y);
+                    break;
+                default:
+                    break;
+            }
+            for (int i = topLeft.x; i < (topLeft.x + 3); i++) {
+                for (int j = topLeft.y; j < (topLeft.y + 3); j++) {
+                    grid[j][i]= '*';
+                }
+            }
+            for (int r = 0; r < rows; r++) {
+                System.out.println(new String(grid[r]));
+            }
+            inp = sc.next().charAt(0);
+        }
+    }
+}
