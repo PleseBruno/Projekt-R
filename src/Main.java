550f04b015e911ecdc9a8237d0de2cd1ae2798ba
Bruno Pleše <brunoplese0@gmail.com>
2025-10-18 00:32:35 +0200

    proba2

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,12 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
 
-        Player p1 = new Player(0,0,0, 0,0);
+        Player p1 = new Player(0, 0, 0, 0, 0);
 
         int key;
         while ((key = System.in.read()) != 'q') {
             p1.setKeyPressed(key);
             p1.step();
-            for (int i = 0; i < 20; i++)
-            {
-                p1.updatePlayer();
-                System.out.println(p1.getForceX() + " f1 " + p1.getForceY() + " f2 " + p1.getY());
-                Thread.sleep(100);
-                }
-            }
         }
     }
+}
